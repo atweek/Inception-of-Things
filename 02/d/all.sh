1569f0bbeacbb0af248969bfe1bf0ac8d0734991
atweek <ramille655@gmail.com>
2022-09-03 16:08:49 +0300

    create folder to argo

--- 02/d/all.sh
+++
@@ -1,7 +0,0 @@
-# sudo pacman -S kubectl docker
-# https://k3d.io/v5.4.5/#quick-start
-
-sudo k3d cluster create atweek --agents 2 --servers 1
-sudo kubectl cluster-info
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
--- 02/d/start.sh
+++
@@ -1,7 +0,0 @@
-systemctl start docker
-sudo k3d cluster list
-sudo kubectl cluster-info
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-echo "----------------"
-sleep 5
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
